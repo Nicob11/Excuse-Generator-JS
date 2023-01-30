49d7c8e376f01372677b5f07cbf3a7585d8cd8b1
NicoB <nicobasma_@hotmail.com>
2023-01-30 20:20:45 +0000

    ejercicio perfe

---
+++ src/app.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+window.onload = function() {
+  //write your code here
+  document.querySelector("#bottonexcuse").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = randomExcuse();
+  });
+
+  const randomExcuse = () => {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
+
+    let excusa =
+      who[randomWho] +
+      " " +
+      action[randomAction] +
+      " " +
+      what[randomWhat] +
+      " " +
+      when[randomWhen];
+
+    return excusa;
+  };
+};
